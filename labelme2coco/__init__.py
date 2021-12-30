876eb2efaf89c8f4d475cc58eec0f3d31c67cd3d
fcakyon <34196005+fcakyon@users.noreply.github.com>
2021-12-31 01:10:41 +0300

    update export name

--- labelme2coco/__init__.py
+++ labelme2coco/__init__.py
@@ -41,6 +41,6 @@ def convert(
         save_json(result["val_coco"].json, save_path)
         logger.info(f"Validation split in COCO format is exported to {save_path}")
     else:
-        save_path = str(Path(export_dir) / "train.json")
+        save_path = str(Path(export_dir) / "dataset.json")
         save_json(coco.json, save_path)
         logger.info(f"Converted annotations in COCO format is exported to {save_path}")
